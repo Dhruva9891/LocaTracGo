fcc9abc4c3c92b2b88014f381bb17787f681a235
Dhruva9891 <dhruvabeti@gmail.com>
2021-04-24 00:39:03 +0530

    Implemented google maps view with search places functionality - Part 1

--- LocaTracGo/AppDelegate.swift
+++ LocaTracGo/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import GooglePlaces
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyCk1i9PMFZ9UqhVUqRzajlZ2__tGbMdt48")
+        GMSPlacesClient.provideAPIKey("AIzaSyCk1i9PMFZ9UqhVUqRzajlZ2__tGbMdt48")
         return true
     }
 
