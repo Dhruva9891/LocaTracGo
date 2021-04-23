33a34edb180fa610cb6e4ed49a8e86fe39f032c2
Dhruva9891 <dhruvabeti@gmail.com>
2021-04-23 23:01:16 +0530

    Initial Commit

---
+++ LocaTracGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocaTracGo
+//
+//  Created by Dhruva Beti on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
